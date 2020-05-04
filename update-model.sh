db1b2b472308e6b41e42012f1352523c7bfa5fa5
Vladimir Tyrin <vtyrin@in-technology.de>
2020-05-04 17:37:53 +0700

    Add openApi model generator

---
+++ update-model.sh
@@ -0,0 +1,7 @@
+output_dir=`dirname $0`/src/model
+echo "Generating model based on swagger specs to $output_dir" 
+rm -rf $output_dir
+openapi-generator generate \
+  --input-spec swagger.yaml \
+  --generator-name typescript-axios \
+  --output $output_dir 
