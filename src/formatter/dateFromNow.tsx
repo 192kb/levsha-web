3e606dec9a42ec83a03071b90dde209d5f26696b
Vladimir Tyrin <vtyrin@in-technology.de>
2020-08-02 20:16:42 +0700

    Fix date formatter

--- src/formatter/dateFromNow.tsx
+++ src/formatter/dateFromNow.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 import moment from 'moment';
 
 export const DateFromNow: React.FC<{ value: string }> = ({ value }) =>
-  value ? <span className='date'>{moment(value).fromNow()} ₽</span> : null;
+  value ? <span className='date'>{moment(value).fromNow()}</span> : null;
 
 export const getPriceText = (value: string) => moment(value).fromNow();
